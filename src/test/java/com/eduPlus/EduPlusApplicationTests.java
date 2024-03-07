bc3c58a5b5cf081b807ebebbd853271b7b6aeaa8
JuanseCho <js.chocont@yahoo.com>
2024-03-07 16:18:04 -0500

    V.0.0.1_Estructurainicial_07032024

---
+++ src/test/java/com/eduPlus/EduPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
