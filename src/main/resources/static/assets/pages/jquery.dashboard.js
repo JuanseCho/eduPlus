765f85dad210de00d782876faa67c69689139607
JuanseCho <js.chocont@yahoo.com>
2024-03-15 13:09:43 -0500

    C.0.0.1_CreacionDeLaVistaDeAdministrador_15032024

---
+++ src/main/resources/static/assets/pages/jquery.dashboard.js
@@ -0,0 +1,17 @@
+/**
+* Theme: Adminox Dashboard
+* Author: Coderthemes
+* Dashboard
+*/
+
+jQuery(function($) {
+
+  'use strict';
+
+    /******************************
+   initialize respective scripts
+   *****************************/
+
+  $(window).load(function() {});
+
+});
